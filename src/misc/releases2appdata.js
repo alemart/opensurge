99e59c959c1e4b8245bc195f8d2dac8271e3e2eb
alemart <alemartf@gmail.com>
2020-01-29 23:09:17 -0300

    appdata stuff

--- src/misc/releases2appdata.js
+++ src/misc/releases2appdata.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 // -----------------------------------------------------------------------------
-// File: releases2xml.js
+// File: releases2appdata.js
 // Description: converts a Release Notes file to an appdata XML block
 // Author: Alexandre Martins <http://opensurge2d.org>
 // License: MIT
 // -----------------------------------------------------------------------------
 
 // How to use:
-// ./releases2xml.js < /path/to/CHANGES.md
+// ./releases2appdata.js < /path/to/CHANGES.md
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin);
 parse(rl).then(printXML);
@@ -23,7 +23,7 @@ function parse(rl)
         rl.on("line", line => {
             // new release
             if(/\d\.\d.*\d{4}\s*$/.test(line)) {
-                let param = match(line, /(\d\.\d(\.\d(\.\d)?)?)\W*(.*)$/);
+                const param = match(line, /(\d\.\d(\.\d(\.\d)?)?)\W*(.*)$/);
                 release = {
                     version: param(1),
                     date: new Date(param(4).replace(/(\d)(st|nd|tr|th)/gi, "$1").trim()),
@@ -34,8 +34,12 @@ function parse(rl)
 
             // release item
             else if(release && /^\s*\*/.test(line)) {
-                let param = match(line, /^\s*\*\s+(.*)$/);
-                release.items.push(param(1).trim());
+                const param = match(line, /^\s*\*\s+(.*)$/);
+                release.items.push(param(1).replace(/(\W)/g, (m, x) => ({
+                    "&": "&amp;",
+                    "<": "&lt;",
+                    ">": "&gt;",
+                })[x] || x).trim());
             }
         });
 
@@ -49,9 +53,9 @@ function parse(rl)
 function printXML(releases)
 {
     const day = d => d.toISOString().substr(0, 10);
-    const indent = n => s => (new Array(n).fill(" ").join("")) + s;
+    const indent = n => s => (new Array(n).fill("  ").join("")) + s;
     const print = n => { const i = indent(n); return s => console.log(i(s)) };
-    const p1 = print(2), p2 = print(4), p3 = print(6), p4 = print(8), p5 = print(10);
+    const p1 = print(1), p2 = print(2), p3 = print(3), p4 = print(4), p5 = print(5);
 
     p1(`<releases>`);
     for(const release of releases) {
