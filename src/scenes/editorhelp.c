2b89358f6cc5787425cefa4df7e42456f1bb6383
alemart <alemartf@gmail.com>
2020-09-07 01:36:21 -0300

    The editor help screen is now translatable

--- src/scenes/editorhelp.c
+++ src/scenes/editorhelp.c
@@ -1,7 +1,7 @@
 /*
  * Open Surge Engine
  * editorhelp.c - level editor help
- * Copyright (C) 2011, 2018  Alexandre Martins <alemartf@gmail.com>
+ * Copyright (C) 2011, 2018, 2020  Alexandre Martins <alemartf@gmail.com>
  * http://opensurge2d.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,6 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdbool.h>
 #include "editorhelp.h"
 #include "../core/font.h"
 #include "../core/scene.h"
@@ -29,44 +30,52 @@
 #include "../core/input.h"
 #include "../entities/sfx.h"
 
+/* private stuff */
+const int COLUMN_SPACING = 96;
+const int MAX_COLUMN_WIDTH = 256;
+const int MIN_PREFERRED_WINDOW_HEIGHT = 720;
+const int MIN_WINDOW_HEIGHT = 480;
 
-/* private data */
-#define BOX_WIDTH               615
-#define BOX_HEIGHT              360
-#define BOX_XPOS                ((VIDEO_SCREEN_W - BOX_WIDTH) / 2)
-#define BOX_YPOS                ((VIDEO_SCREEN_H - BOX_HEIGHT) / 2)
-#define BOX_PADDING             15
-
-static font_t *title, *label, *quitlabel;
+static font_t *columnfnt[2], *errorfnt;
 static image_t *background;
 static input_t *in;
 
 static const char text[] = 
-"<color=ff8060>General</color>\n\n"
-"F12                                Return to the game\n"
-"1 | 2                              Open palette: brick | entity\n"
-"Ctrl + S                           Save the level\n"
-"Ctrl + R                           Reload the level\n"
-"Arrow keys | WASD                  Move the camera\n"
-"Shift + Arrows | Shift + WASD      Move the camera (faster)\n"
-"Ctrl + Z | Ctrl + Y                Undo | Redo\n"
-"F1                                 Show help\n"
-"G                                  Snap to grid\n"
-"M                                  Show collision masks & gizmos\n"
-"\n\n"
-"<color=ff8060>Item placement</color>\n\n"
-"Left mouse button                  Put item\n"
-"Middle mouse button                Pick item\n"
-"Right mouse button                 Erase item (hold: eraser)\n"
-"Mouse wheel                        Change the current item\n"
-"Shift + Left mouse button          Change the level of the water\n"
-"Ctrl + Left mouse button           Change the spawn point\n"
-"L | Shift + L                      Change brick layer\n"
-"F | Shift + F                      Flip brick\n"
-"Shift + Mouse wheel                Change item type (legacy)\n"
-"\n\n"
-"<color=ff8060>Layer system</color>: yellow L (see palette) enables yellow layer and disables green. Green L enables green layer and disables yellow. Green appears in front of yelllow. Change brick layers with L.\n"
+"$EDITOR_HELP_GENERAL                 [ $EDITOR_HELP_BACK ]\n"
+"\n"
+"F12                                  $EDITOR_HELP_CMD_RETURN\n"
+"1 | 2                                $EDITOR_HELP_CMD_PALETTE\n"
+"$EDITOR_HELP_INPUT_CTRL + S          $EDITOR_HELP_CMD_SAVE\n"
+"$EDITOR_HELP_INPUT_CTRL + R          $EDITOR_HELP_CMD_RELOAD\n"
+"$EDITOR_HELP_INPUT_ARROWS | WASD     $EDITOR_HELP_CMD_MOVE\n"
+"$EDITOR_HELP_INPUT_SHIFT + $EDITOR_HELP_INPUT_ARROWS | $EDITOR_HELP_INPUT_SHIFT + WASD     $EDITOR_HELP_CMD_MOVEFASTER\n"
+"$EDITOR_HELP_INPUT_CTRL + Z | $EDITOR_HELP_INPUT_CTRL + Y                                  $EDITOR_HELP_CMD_UNDO | $EDITOR_HELP_CMD_REDO\n"
+"F1                                   $EDITOR_HELP_CMD_HELP\n"
+"G                                    $EDITOR_HELP_CMD_GRID\n"
+"M                                    $EDITOR_HELP_CMD_MASKS\n"
+"\n"
+"$EDITOR_HELP_ITEMS\n"
+"\n"
+"$EDITOR_HELP_INPUT_LEFTCLICK                                     $EDITOR_HELP_CMD_PUTITEM\n"
+"$EDITOR_HELP_INPUT_MIDDLECLICK                                   $EDITOR_HELP_CMD_PICKITEM\n"
+"$EDITOR_HELP_INPUT_RIGHTCLICK                                    $EDITOR_HELP_CMD_DELETEITEM\n"
+"$EDITOR_HELP_INPUT_MOUSEWHEEL                                    $EDITOR_HELP_CMD_CHANGEITEM\n"
+"$EDITOR_HELP_INPUT_SHIFT + $EDITOR_HELP_INPUT_LEFTCLICK          $EDITOR_HELP_CMD_WATERLEVEL\n"
+"$EDITOR_HELP_INPUT_CTRL + $EDITOR_HELP_INPUT_LEFTCLICK           $EDITOR_HELP_CMD_SPAWNPOINT\n"
+"L | $EDITOR_HELP_INPUT_SHIFT + L                                 $EDITOR_HELP_CMD_BRICKLAYER\n"
+"F | $EDITOR_HELP_INPUT_SHIFT + F                                 $EDITOR_HELP_CMD_FLIPBRICK\n"
+"$EDITOR_HELP_INPUT_SHIFT + $EDITOR_HELP_INPUT_MOUSEWHEEL         $EDITOR_HELP_CMD_CHANGETYPE\n"
+"\n"
+"$EDITOR_HELP_LAYERS\n"
+"\n"
+"$EDITOR_HELP_LAYERSTUTORIAL"
 ;
+
+static char text_column[2][sizeof(text)] = { "", "" };
+
+static void split_columns(const char *text, char *col1, char *col2, bool save_space);
+static void compute_box_dimensions(v2d_t col1size, v2d_t col2size, int *xpos, int *ypos, int *width, int *height);
+
 
 
 /*
@@ -75,32 +84,50 @@ static const char text[] =
  */
 void editorhelp_init(void *foo)
 {
-    title = font_create("default");
-    font_set_text(title, "<color=80ff60>LEVEL EDITOR</color>");
-    font_set_position(title, v2d_new(BOX_XPOS + (BOX_WIDTH - font_get_textsize(title).x) / 2, BOX_YPOS + BOX_PADDING));
-    font_set_visible(title, FALSE);
+    v2d_t window_size = video_get_window_size();
+    int box_xpos, box_ypos, box_width, box_height;
 
-    label = font_create("default");
-    font_set_position(label, v2d_new(BOX_XPOS + BOX_PADDING, BOX_YPOS + BOX_PADDING));
-    font_set_width(label, BOX_WIDTH - BOX_PADDING * 2);
-    font_set_text(label, "%s", text);
-    if(video_get_window_size().x < BOX_WIDTH) {
-        font_set_position(label, v2d_new(BOX_PADDING, BOX_PADDING));
-        font_set_width(label, video_get_window_size().x - BOX_PADDING * 2);
-        font_set_text(label,
-            "<color=ff8060>LEVEL EDITOR</color>\n\n"
-            "Please increase the window size on the options screen and try again.\n\n"
-            "Press <color=ff8060>ESC</color> to go back."
-        );
-    }
-
-    quitlabel = font_create("default");
-    font_set_text(quitlabel, "[press <color=ff8060>ESC</color>]");
-    font_set_position(quitlabel, v2d_new(BOX_XPOS + BOX_WIDTH - BOX_PADDING - font_get_textsize(quitlabel).x, BOX_YPOS + BOX_PADDING));
-
+    /* setup background & input device */
     background = image_clone(video_get_backbuffer());
-
     in = input_create_user("editorhelp");
+
+    /* configure the text columns */
+    columnfnt[0] = font_create("EditorUI");
+    columnfnt[1] = font_create("EditorUI");
+    split_columns(text, text_column[0], text_column[1], (int)window_size.y < MIN_PREFERRED_WINDOW_HEIGHT);
+    font_set_text(columnfnt[0], "%s", text_column[0]);
+    font_set_text(columnfnt[1], "%s", text_column[1]);
+    font_set_width(columnfnt[0], MAX_COLUMN_WIDTH * 3);
+    font_set_width(columnfnt[1], MAX_COLUMN_WIDTH);
+
+    /* position the columns */
+    compute_box_dimensions(
+        font_get_textsize(columnfnt[0]), font_get_textsize(columnfnt[1]),
+        &box_xpos, &box_ypos, &box_width, &box_height
+    );
+    font_set_position(columnfnt[0], v2d_new(
+        box_xpos,
+        box_ypos
+    ));
+    font_set_position(columnfnt[1], v2d_new(
+        box_xpos + MAX_COLUMN_WIDTH + COLUMN_SPACING,
+        box_ypos
+    ));
+
+    /* setup the error font (the window is tiny) */
+    errorfnt = font_create("EditorUI");
+    if((int)window_size.y < MIN_WINDOW_HEIGHT) {
+        const int PADDING = 8;
+        font_set_position(errorfnt, v2d_new(PADDING, PADDING));
+        font_set_width(errorfnt, video_get_window_size().x - PADDING * 2);
+        font_set_text(errorfnt, "$EDITOR_HELP_TINYWINDOW");
+        font_set_visible(columnfnt[0], false);
+        font_set_visible(columnfnt[1], false);
+    }
+    else
+        font_set_visible(errorfnt, false);
+
+    /* a nice touch */
     sound_play(SFX_CONFIRM);
 }
 
@@ -112,6 +139,7 @@ void editorhelp_init(void *foo)
  */
 void editorhelp_update()
 {
+    /* go back */
     if(input_button_pressed(in, IB_FIRE1)) {
         scenestack_pop();
         return;
@@ -126,13 +154,13 @@ void editorhelp_update()
  */
 void editorhelp_render()
 {
-    v2d_t v = v2d_new(VIDEO_SCREEN_W/2, VIDEO_SCREEN_H/2);
+    v2d_t cam = v2d_new(VIDEO_SCREEN_W/2, VIDEO_SCREEN_H/2);
+
     image_clear(color_rgb(18, 18, 18));
-    image_draw_trans(background, 0, 0, 0.15f, IF_NONE);
-    image_rectfill(BOX_XPOS, BOX_YPOS, BOX_XPOS + BOX_WIDTH - 1, BOX_YPOS + BOX_HEIGHT - 1, color_rgb(40, 44, 52));
-    font_render(title, v);
-    font_render(label, v);
-    font_render(quitlabel, v);
+    image_draw_trans(background, 0, 0, 0.1f, IF_NONE);
+    font_render(columnfnt[0], cam);
+    font_render(columnfnt[1], cam);
+    font_render(errorfnt, cam);
 }
 
 
@@ -143,11 +171,59 @@ void editorhelp_render()
  */
 void editorhelp_release()
 {
-    sound_play(SFX_BACK);
-
+    /* release resources */
     input_destroy(in);
     image_destroy(background);
-    font_destroy(quitlabel);
-    font_destroy(label);
-    font_destroy(title);
+    font_destroy(errorfnt);
+    font_destroy(columnfnt[1]);
+    font_destroy(columnfnt[0]);
+
+    /* a nice touch */
+    sound_play(SFX_BACK);
+}
+
+
+/* private stuff */
+
+/* split the text into two columns */
+void split_columns(const char *text, char *col1, char *col2, bool save_space)
+{
+    const int priority_sections = 2;
+    const char *p = text;
+    char **q = &col1;
+    int k = priority_sections * 2;
+
+    while(*p) {
+        if(*p == '\n') {
+            /* new line */
+            q = &col1;
+            *col1++ = *col2++ = *p++;
+            if(save_space && *p == '\n' && !(--k))
+                break; /* discard text - need to save space */
+            continue;
+        }
+        else if(*p == ' ' && *(p+1) == ' ') {
+            /* change column */
+            q = &col2;
+            while(*p == ' ')
+                p++;
+            continue;
+        }
+
+        /* copy character */
+        *((*q)++) = *p++;
+    }
+
+    *col1 = *col2 = '\0';
+}
+
+/* compute a bounding box surrounding the text */
+void compute_box_dimensions(v2d_t col1size, v2d_t col2size, int *xpos, int *ypos, int *width, int *height)
+{
+    const int COLUMN_HEIGHT[2] = { (int)(col1size.y), (int)(col2size.y) };
+
+    *width = MAX_COLUMN_WIDTH * 2 + COLUMN_SPACING;
+    *height = max(COLUMN_HEIGHT[0], COLUMN_HEIGHT[1]);
+    *xpos = (VIDEO_SCREEN_W - *width) / 2;
+    *ypos = (VIDEO_SCREEN_H - *height) / 2;
 }
