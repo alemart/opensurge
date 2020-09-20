a9ab17c048e3b52a9af8ad40231b80e3d2dbc08c
alemart <alemartf@gmail.com>
2020-09-20 01:09:56 -0300

    Will recognize untranslated entries

--- src/core/lang.h
+++ src/core/lang.h
@@ -1,7 +1,7 @@
 /*
  * Open Surge Engine
  * lang.h - language/translation module
- * Copyright (C) 2009, 2019  Alexandre Martins <alemartf@gmail.com>
+ * Copyright (C) 2009, 2019-2020  Alexandre Martins <alemartf@gmail.com>
  * http://opensurge2d.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -32,7 +32,7 @@ char* lang_getstring(const char* desired_key, char* dest, size_t dest_size);
 const char* lang_get(const char* desired_key);
 const char* lang_getid();
 bool lang_haskey(const char* desired_key);
-void lang_compatibility(const char* filename, int* supver, int* subver, int* wipver);
+void lang_compatibility(const char* filepath, int* supver, int* subver, int* wipver);
 char* lang_metadata(const char* filepath, const char* desired_key, char* dest, size_t dest_size);
 
 #endif
