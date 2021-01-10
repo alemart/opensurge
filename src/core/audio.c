8e895468bb27011f1f27c80071f0156801a6875d
alemart <alemartf@gmail.com>
2021-01-10 11:48:56 -0300

    Increase PREFERRED_NUMBER_OF_SAMPLES to 32

--- src/core/audio.c
+++ src/core/audio.c
@@ -53,7 +53,7 @@ struct sound_t {
 };
 
 /* private stuff */
-static const int PREFERRED_NUMBER_OF_SAMPLES = 16; /* how many samples can be played at the same time */
+static const int PREFERRED_NUMBER_OF_SAMPLES = 32; /* how many samples can be played at the same time */
 static music_t *current_music = NULL; /* music being played at the moment (NULL if none) */
 
 #elif !defined(__USE_OPENAL__)
