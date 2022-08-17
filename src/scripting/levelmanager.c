b76ba77bc11e1a9689451bf4817e213158d614d4
alemart <alemartf@gmail.com>
2022-08-16 21:07:07 -0300

    Comment unused variable

--- src/scripting/levelmanager.c
+++ src/scripting/levelmanager.c
@@ -106,9 +106,8 @@ surgescript_var_t* fun_onlevelunload(surgescript_object_t* object, const surgesc
     surgescript_heap_t* heap = surgescript_object_heap(object);
     surgescript_objectmanager_t* manager = surgescript_object_manager(object);
     surgescript_object_t* level = surgescript_objectmanager_get(manager, surgescript_var_get_objecthandle(surgescript_heap_at(heap, LEVEL_ADDR)));
-    surgescript_object_t* playermanager = surgescript_objectmanager_get(manager, surgescript_var_get_objecthandle(surgescript_heap_at(heap, PLAYERMANAGER_ADDR)));
-    surgescript_object_call_function(level, "__callUnloadFunctor", NULL, 0, NULL); /* call Level.onUnload(), if applicable */
 
+    surgescript_object_call_function(level, "__callUnloadFunctor", NULL, 0, NULL); /* call Level.onUnload(), if applicable */
     surgescript_object_kill(level); /* destroy the Level, as well as its objects */
     surgescript_var_set_null(surgescript_heap_at(heap, LEVEL_ADDR)); /* nobody can access the Level now (*) */
 
@@ -137,6 +136,7 @@ surgescript_var_t* fun_onlevelunload(surgescript_object_t* object, const surgesc
     Since PlayerManager is not a child of the Level object, it won't be affected when
     the latter is destroyed.
 
+    //surgescript_object_t* playermanager = surgescript_objectmanager_get(manager, surgescript_var_get_objecthandle(surgescript_heap_at(heap, PLAYERMANAGER_ADDR)));
     //surgescript_object_kill(playermanager);
     //surgescript_var_set_null(surgescript_heap_at(heap, PLAYERMANAGER_ADDR));
     */
