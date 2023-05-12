173bc247229f7429e88cfa428beb76b0ecf19c0e
alemart <alemartf@gmail.com>
2023-05-11 23:40:05 -0300

    EntityEvent: use Level.findEntity() and Level.findEntities()

--- src/scripting/events.c
+++ src/scripting/events.c
@@ -74,10 +74,10 @@ object 'EntityEvent' is 'event' \n\
             else \n\
                 Console.print(this.__name + ': undefined function ' + method); \n\
         } \n\
-        else if((entity = Level.child(target)) !== null) { " /* select by entity name */ " \n\
+        else if((entity = Level.findEntity(target)) !== null) { " /* select by entity name */ " \n\
             if(entity.hasFunction(method)) { \n\
                 if(entity.__arity(method) == params.length) { \n\
-                    entities = Level.children(target); \n\
+                    entities = Level.findEntities(target); \n\
                     length = entities.length; \n\
                     for(i = 0; i < length; i++) { \n\
                         entity = entities[i]; \n\
