c0e2f0739edbfc4349eca385035ef1ab29bae98f
alemart <alemartf@gmail.com>
2023-06-24 21:06:19 -0300

    Add rect_t and point_t utilities

---
+++ src/util/point.h
@@ -0,0 +1,51 @@
+/*
+ * Open Surge Engine
+ * point.h - 2D points with integer coordinates
+ * Copyright (C) 2008-2023  Alexandre Martins <alemartf@gmail.com>
+ * http://opensurge2d.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _POINT_H
+#define _POINT_H
+
+/* Point struct */
+typedef struct point_t {
+    int x;
+    int y;
+} point_t;
+
+/* Constructor */
+#define point_new(x, y) (point_t){ (x), (y) }
+
+/* Equality test */
+#define point_equals(p1, p2) ((p1) == (p2))
+
+/* Addition p1 + p2 */
+#define point_add(p1, p2) (point_t){ \
+    (p1).x + (p2).x, \
+    (p1).y + (p2).y \
+}
+
+/* Subtraction p1 - p2 */
+#define point_subtract(p1, p2) (point_t){ \
+    (p1).x - (p2).x, \
+    (p1).y - (p2).y \
+}
+
+/* Dot product */
+#define point_dot(p1, p2) ((p1).x * (p2).x + (p1).y * (p2).y)
+
+#endif
---
+++ src/util/rect.h
@@ -0,0 +1,56 @@
+/*
+ * Open Surge Engine
+ * rect.h - rectangles with integer coordinates
+ * Copyright (C) 2008-2023  Alexandre Martins <alemartf@gmail.com>
+ * http://opensurge2d.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _RECT_H
+#define _RECT_H
+
+/* Rectangle struct */
+typedef struct rect_t {
+    int x;
+    int y;
+    int width;
+    int height;
+} rect_t;
+
+/* Constructor */
+#define rect_new(x, y, width, height) (rect_t){ (x), (y), (width), (height) }
+
+/* Area of the rectangle */
+#define rect_area(r) ((r).width * (r).height)
+
+/* Equality test */
+#define rect_equals(r1, r2) ((r1) == (r2))
+
+/* Does rectangle r contains point p?
+   p is a point_t or a v2d_t */
+#define rect_contains(r, p) ( \
+    (p).x >= (r).x && (p).x < (r).x + (r).width && \
+    (p).y >= (r).y && (p).y < (r).y + (r).height \
+)
+
+/* Do rectangles r1 and r2 overlap? */
+#define rect_overlaps(r1, r2) !( \
+    (r1).x >= (r2).x + (r2).width || \
+    (r2).x >= (r1).x + (r1).width || \
+    (r1).y >= (r2).y + (r2).height || \
+    (r2).y >= (r1).y + (r1).height \
+)
+
+#endif
