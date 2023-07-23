42a69a9581c5d8ea80c4f45641da35fe6014a205
alemart <alemartf@gmail.com>
2023-07-23 19:35:28 -0300

    Disable camera boundaries in the Debug Mode

--- src/entities/camera.c
+++ src/entities/camera.c
@@ -80,7 +80,7 @@ void camera_update()
     v2d_t ds;
 
     /* updating the boundaries */
-    if(level_editmode()) /* no boundaries in the editor */
+    if(level_editmode() || level_is_in_debug_mode()) /* no boundaries in the editor */
         disable_boundaries();
     else if(!camera.is_locked) /* the level size may have changed since the last frame */
         reset_boundaries();
