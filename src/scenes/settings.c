dd7604e5567f03112c8c0330c74adc7f521e0aea
alemart <alemartf@gmail.com>
2024-01-10 22:44:47 -0300

    Change progress bar (update rate)

--- src/scenes/settings.c
+++ src/scenes/settings.c
@@ -1642,13 +1642,15 @@ bool download_to_cache(ALLEGRO_FILE* f, const char* destination_path, void (*on_
 {
     /* since we're operating on the application cache, we have
        write permissions to open the destination path for writing */
-    uint8_t buffer[4096];
+    enum { BUFFER_SIZE = 4096, MEGABYTE = 1048576, PROGRESS_CHUNK = 1 * MEGABYTE };
+    typedef char __chunksize_assertion[-1 + 2 * (!!(PROGRESS_CHUNK % BUFFER_SIZE == 0))];
+
+    uint8_t buffer[BUFFER_SIZE];
     size_t n;
     bool error;
     FILE* f_copy;
     int64_t total_bytes;
     int64_t bytes_written;
-    const int64_t PROGRESS_CHUNK = sizeof(buffer) * 1024;
 
     /* open filepath for writing */
     if(NULL == (f_copy = fopen(destination_path, "wb"))) {
