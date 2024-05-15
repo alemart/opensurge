732e13d26d7ba1f6b310e6b752c6a46b60ca34df
alemart <221477+alemart@users.noreply.github.com>
2024-05-14 22:25:54 -0300

    Move css and js to separate files

---
+++ inc/home.js
@@ -0,0 +1,18 @@
+/* reveal on scroll */
+window.addEventListener('load', function() {
+    if(typeof IntersectionObserver === "undefined")
+        return;
+
+    const observer = new IntersectionObserver(function(entries) {
+        for(const entry of entries) {
+            if(entry.intersectionRatio >= 0.5)
+                entry.target.classList.remove("unrevealed");
+            else if(entry.intersectionRatio == 0.0)
+                entry.target.classList.add("unrevealed");
+        }
+    }, { threshold: [0, 0.5] });
+
+    document.querySelectorAll(".reveal").forEach(function(reveal) {
+        observer.observe(reveal);
+    });
+});
